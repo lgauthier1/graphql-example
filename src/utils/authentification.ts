c0ea15fa3fca3a5969883c1e4314f266693c5d95
Laurent <gauthier.lrnt@gmail.com>
2023-03-11 19:01:55 +0100

    fix: last esling warning

--- src/utils/authentification.ts
+++ src/utils/authentification.ts
@@ -2,19 +2,25 @@ import { User } from '@prisma/client'
 import jwt from 'jsonwebtoken'
 import { TokenType } from './types'
 
+// TODO LOG WARNING MISSING SECRET IN .ENV
+const accessTokenSecret =
+  process.env.ACCESS_TOKEN_SECRET || 'defaultUnsecuredAccessTokenSecret'
+const refreshTokenSecret =
+  process.env.refreshTokenSecret || 'defaultUnsecuredRefreshTokenSecret'
+
 export const createToken = (tokenType: TokenType, user: User): string => {
   switch (tokenType) {
     case TokenType.accessToken:
       return (
         'Bearer ' +
-        jwt.sign({ email: user.email }, process.env.ACCESS_TOKEN_SECRET!, {
+        jwt.sign({ email: user.email }, accessTokenSecret, {
           expiresIn: '30s'
         })
       )
     case TokenType.refreshToken:
       return (
         'Bearer ' +
-        jwt.sign({ email: user.email }, process.env.REFRESH_TOKEN_SECRET!, {
+        jwt.sign({ email: user.email }, refreshTokenSecret, {
           expiresIn: '2d'
         })
       )
